f62d2a6dbd067744c021dae61f15aa1aa7480073
kennycaiguo <kennycai8888@outlook.com>
2025-11-04 18:28:43 -0600

    upload excel

--- 04_django/day20/day16app/views/dep_view.py
+++ 04_django/day20/day16app/views/dep_view.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect,HttpResponse
+from django.shortcuts import render, redirect, HttpResponse
 from day16app import models  # 导入models因为以后可能里面会有很多类不能一个一个导入
 from day16app.utils.pagination import Pagination
 from openpyxl import load_workbook
@@ -9,7 +9,7 @@ def dep_list(req):
     dep_list = models.Department.objects.all()
     # 分页功能实现
     # 1.实例化分页组件
-    page_obj = Pagination(req, dep_list, page_size=2)
+    page_obj = Pagination(req, dep_list, page_size=8)
     # 2.获取分页后的数据
     page_query_set = page_obj.page_queryset
     # 3.获取分页器的html字符串
@@ -40,7 +40,7 @@ def dep_edit(req, nid):
     # 处理get
     # 保证nid存在
     if not models.Department.objects.filter(id=nid).exists():
-        return redirect("/dep/list")     # 方式1 重定向
+        return redirect("/dep/list")  # 方式1 重定向
         # return render(req,"error.html")   # 方式2 展示错误页面
     if req.method == "GET":
         dep = models.Department.objects.filter(id=nid).first()
@@ -53,16 +53,18 @@ def dep_edit(req, nid):
 
 
 def dep_multiadd(req):
-    print(req.FILES)
+    # 1.获取用户上传的文件,得到一个文件对象
     exc = req.FILES.get("exc")
     print(exec)
-    # f = open("media\\" + exc.name, mode='wb')
-    # for chunk in exc.chunks():
-    #     f.write(chunk)
-    # f.close()
-    # 读取用户上传的excel文件并且保存数据
-    wb = load_workbook(exc)
+    # 2.读取这个文件对象的数据并且保存
+    wb = load_workbook(exc)  # 这个函数不仅仅可以传递一个文件名,也可以传递一个文件对象作为参数,就是这里的exc文件对象.
     # print(wb)
     sheet = wb.worksheets[0]
-    print(sheet)
-    return HttpResponse("上传成功")
+    # 3.我们这个文件笔记特殊,只有一列,我们只需要获取这一列的数据,然后把这一列的每一行的数据保存到数据库即可.注意必须从2开始因为第一行是表头,我们不需要
+    # 这个包裹只有一列,所以比较简单.
+    for row in sheet.iter_rows(min_row=2):
+        title1 = row[0].value
+        # 先查询一下有没有这个部门,如果有,就不创建
+        if not models.Department.objects.filter(title=title1).exists():
+            models.Department.objects.create(title=title1)
+    return redirect("/dep/list/")
